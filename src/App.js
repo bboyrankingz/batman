75697c0347698c7c21e200a1c0746c799a83e8ff
bboy.stats <bboy.stats@gmail.com>
2018-10-12 22:49:58 +0200

    footage by fetching bboyrankingz website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Banner from './Banner';
+import Footage from './Footage';
 
 class App extends Component {
     render() {
@@ -8,6 +9,7 @@ class App extends Component {
             <div>
                 <Header/>
                 <Banner/>
+                <Footage/>
             </div>
         );
     }
---
+++ src/Footage.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import {
+    Container,
+    Row,
+} from 'reactstrap';
+import './Footage.css';
+
+
+class Footage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            results: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://bboyrankingz.com/media/search/bruce%20wayne.json')
+            .then(response => response.json())
+            .then(data => this.setState({ results: data.results }));
+    }
+
+    render() {
+
+        const { results } = this.state;
+
+        return (
+            <div>
+                <section id="blog" class="section-full">
+                    <Container>
+                        <Row className="justify-content-center">
+
+                            <div class="col-lg-8">
+                                <div class="product-area-title text-center">
+                                    <p class="text-uppercase">Latest From me</p>
+                                    <h2 class="h1">Footage of my last battle, workshop, showcase...</h2>
+                                </div>
+                            </div>
+                        </Row>
+
+                        <Row>
+                            {results.map(footage =>
+                                <div className="col-lg-3 col-md-6">
+                                    <div className="single-publish">
+                                        <img src={footage.thumbnail} className="img-fluid" alt="" />
+                                        <div className="top">
+                                            <div className="mb-15 d-flex">
+                                                <a href="#">{footage.created}</a>
+                                                <span className="line">|</span>
+                                                <a href="#">By {footage.channel_title}</a>
+                                            </div>
+                                            <h6 className="text-uppercase"><a href={footage.url}>{footage.title}</a></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </Row>
+                    </Container>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Footage;
