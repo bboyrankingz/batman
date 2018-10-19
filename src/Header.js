a07b8f7d27cef389cba6ba1e83841ac1f6cdca87
bboy.stats <bboy.stats@gmail.com>
2018-10-20 00:11:42 +0200

    sticky header

--- src/Header.js
+++ src/Header.js
@@ -3,35 +3,44 @@ import {
     Container,
 } from 'reactstrap';
 import './Header.css';
+import 'react-sticky-header/styles.css';
+import StickyHeader from 'react-sticky-header';
+
 
 class Header extends Component {
+
     render() {
         return (
             <div>
-                <header className="default-header">
-                    <div className="sticky-header">
-                        <Container>
-                            <div className="header-content d-flex justify-content-between align-items-center">
-                                <div className="right-bar d-flex align-items-center">
-                                    <nav className="d-flex align-items-center">
-                                        <ul className="main-menu">
-                                            <li><a href="#about">Bruce Wayne</a></li>
-                                            <li><a href="#studio">Break ma Baraque</a></li>
-                                            <li><a href="#footage">Footage</a></li>
-                                        </ul>
-                                        <a href="#" className="mobile-btn"><span className="lnr lnr-menu"></span></a>
-                                    </nav>
-                                    <div className="header-social d-flex align-items-center">
-                                        <a href="."><i className="fa fa-facebook"></i></a>
-                                        <a href="."><i className="fa fa-twitter"></i></a>
-                                        <a href="."><i className="fa fa-linkedin"></i></a>
+                <StickyHeader
+                    // This is the sticky part of the header.
+                    header={
+                        <header className="default-header">
+                            <div className="sticky-header">
+                                <Container>
+                                    <div className="header-content d-flex justify-content-between align-items-center">
+                                        <div className="right-bar d-flex align-items-center">
+                                            <nav className="d-flex align-items-center">
+                                                <ul className="main-menu">
+                                                    <li><a href="#about">Bruce Wayne</a></li>
+                                                    <li><a href="#studio">Break ma Baraque</a></li>
+                                                    <li><a href="#footage">Footage</a></li>
+                                                </ul>
+                                                <a href="#" className="mobile-btn"><span className="lnr lnr-menu"></span></a>
+                                            </nav>
+                                            <div className="header-social d-flex align-items-center">
+                                                <a href="."><i className="fa fa-facebook"></i></a>
+                                                <a href="."><i className="fa fa-twitter"></i></a>
+                                                <a href="."><i className="fa fa-linkedin"></i></a>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
+                                </Container>
                             </div>
-                        </Container>
-                    </div>
-                </header>
-
+                        </header>
+                    }
+                >
+                </StickyHeader>
             </div>
         );
     }
