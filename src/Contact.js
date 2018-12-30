d7be6de0f8877d275299a2a375ac477462038fb4
bboy.stats <bboy.stats@gmail.com>
2018-12-30 18:27:54 +0100

    Update some css details and contact info

--- src/Contact.js
+++ src/Contact.js
@@ -11,27 +11,26 @@ export class Contact extends Component {
                         <div className="col-lg-8">
                             <div className="product-area-title text-center">
                                 <p className="text-uppercase">Contact me</p>
-                                <h2 className="h1">bruce wayne aka mathieu...</h2>
+                                <h2 className="h1">contact@breakmabaraque.fr</h2>
                             </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-lg-6 mt-30">
-                            <p>blablbala.</p>
                             <div className="row">
                                 <div className="col-sm-6">
                                     <div className="address mt-20">
-                                        <h6 className="text-uppercase mb-15">Physical Address</h6>
-                                        <p>Emperor Publishing,56/8, bir uttam qazi nuruzzaman road, Dhaka - 1205</p>
+                                        <h6 className="text-uppercase mb-15">Adresse</h6>
+                                        <p>89 rue Rebeval 75019, Paris</p>
                                     </div>
                                 </div>
                                 <div className="col-sm-6">
                                     <div className="address mt-20">
                                         <h6 className="text-uppercase mb-15">Telephone</h6>
-                                        <a href="tel:0000">012 (88) 017 25 656 565</a>
-                                        <a href="tel:0000">012 (88) 017 25 656 565</a>
+                                        <a href="tel:0000">+33 7 61 88 57 54</a>
                                     </div>
                                 </div>
+                            
                             </div>
                         </div>
                         <div className="col-lg-6 mt-30">
